08325b57b6138d8125bb3f133e71b41e91168316
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-06-11 22:22:34 -0300

    exercício 05

---
+++ src/semana04/exercicio05.sql
@@ -0,0 +1,20 @@
+--[M2S04] - Ex. 05 - Joins
+
+SELECT p.pedido_id, c.cliente_id, c.nome, c.cidade, c.idade
+FROM pedidos p 
+INNER JOIN clientes c ON p.cliente_id = c.cliente_id;
+
+
+------------------------
+
+SELECT c.nome, p.pedido_id
+FROM clientes c
+LEFT JOIN pedidos p ON c.cliente_id = p.cliente_id
+
+------------------------
+
+SELECT p.produto_id, p.nome_produto, ip.pedido_id, ip.quantidade
+FROM itens_pedidos ip
+RIGHT JOIN produtos p ON ip.produto_id = p.produto_id
+
+------------------------
